237728fcc1cefc0d3ef422bf057c04c49e99ec05
maksim.gerasimchuk <Maksim.Gerasimchuk@nexign.com>
2020-04-30 13:51:21 +0300

    #0 created sample project for https (ssl/tls) learn

---
+++ src/test/java/com/example/demohttps/DemoHttpsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demohttps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoHttpsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
